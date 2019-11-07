ebd738efb093384c5621bbefd8bed9e45589b956
unknown <>
2019-11-07 11:15:44 +0800

    第二次征服宇宙

---
+++ UnityProject/Assets/Script/Robot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Robot : MonoBehaviour {
+
+
+    /// <summary>
+    /// 任務對話
+    /// </summary>
+    [Header("對話")]
+    public string Say = "還不快給我拿螺絲來 ";
+    public string ComMission = "沒想到你還挺能幹的 ";
+    public string NoComPMission = "連個螺絲都沒拿過來, 給我打掉重練 ";
+    public float SaySpeed;
+
+    /// <summary>
+    /// 任務相關
+    /// </summary>
+    [Header("任務條件")]
+    public bool 是否完成任務;
+    public int 總需求數量;
+    public int 已達成數量;
+
+}
---
+++ UnityProject/Assets/Script/Robot_Ball.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class Robot_Ball : MonoBehaviour {
+
+    //移動
+    [Header("移動速度"), Range(0.0f, 50.0f)]
+    public float Speed;
+
+    //跳耀
+    [Header("跳躍距離"), Range(0.0f, 100.0f)]
+    public float Jump;
+
+    //是否在地板
+    public bool inGround;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    private Rigidbody Rob3D;
+
+    void Start ()
+    {
+        Rob3D = GetComponent<Rigidbody>();
+        GetComponent<Rigidbody>().position = Vector3.zero;
+    }
+	
+	
+	void Update ()
+    {
+		
+	}
+
+    //Time //呼叫副程式
+    private void FixedUpdate()
+    {
+        RobWalk();
+        RobJump();
+    }
+
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void RobWalk()
+    {
+        Rob3D.AddForce(new Vector3(Speed * Input.GetAxis("Vertical") , 0, Speed * Input.GetAxis("Horizontal")));        
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void RobJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && inGround == true)
+        {
+            inGround = false;
+            Rob3D.AddForce(new Vector3(0, Jump, 0));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        inGround = true;
+    }
+
+}
